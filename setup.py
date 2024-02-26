00f281122d80bc3d179cf1ea88cc8b02a4742efa
Umyam <87502414+umyams@users.noreply.github.com>
2024-02-26 11:25:17 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,5 @@
 from setuptools import setup
 setup(
-    name=heynotbot,
-    version=0.1,
-    description=i_dont_know,
-    long_description=just_trying,
-    author=AUTHOR,
-    author_email=AUTHOR_EMAIL,
-    download_url=DOWNLOAD_URL,
-    license=LICENSE,
-    packages=['mypackage'],
-    install_requires=[
-        'heyreplit==0.8'],
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
   )
