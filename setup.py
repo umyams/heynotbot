ac1b130e73825f140144dfd26b42f91eb2f94b8e
Umyam <87502414+umyams@users.noreply.github.com>
2024-02-26 11:03:47 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,9 @@
 from setuptools import setup
 setup(
+    install_requires=[
+          'heyreplit',
+      ],
+      zip_safe=False)
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
 )
