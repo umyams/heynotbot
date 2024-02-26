980b020612737e315233bdafe2673a366a8bf92d
Umyam <87502414+umyams@users.noreply.github.com>
2024-02-26 11:50:23 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,7 @@ setup(
     name="heynotbot",
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
+    install_requires=[
+        'heyreplit==0.8',
+    ],
   )
