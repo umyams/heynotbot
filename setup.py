7e3c29187b16e4ee985168881a03fd69b589a832
Umyam <87502414+umyams@users.noreply.github.com>
2024-03-05 21:21:43 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,3 @@
-from setuptools import setup
-setup(
-    name="heynotbot",
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    install_requires=[
-        'heyreplit==0.8',
-    ],
-  )
+import setuptools
+
+setuptools.setup()
